ff65d2ec6c5e28335152f8be5ce121bc2819d181
kaito <kaitosatou844@gmail.com>
2023-03-20 11:10:44 +0900

    チャット画面上にアカウントアイコン追加、削除、編集、アップデート機能追加

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:intl/intl.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -63,11 +64,11 @@ class _SignInPageState extends State<SignInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('GoogleSignIn'),
+        title: const Text('Login'),
       ),
       body: Center(
         child: ElevatedButton(
-          child: const Text('GoogleSignIn'),
+          child: const Text('Login'),
           onPressed: () async {
             await signInWithGoogle();
             if(mounted){
@@ -101,69 +102,196 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final controller = TextEditingController();
+ @override
+ void dispose(){
+   super.dispose();
+   controller.dispose();
+ }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('チャット'),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: InkWell(
-              onTap: (){
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context){
-    return const MyPage();
-    },
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('チャット'),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: (){
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context){
+      return const MyPage();
+      },
+                    ),
+                  );
+                },
+                child: CircleAvatar(
+                  backgroundImage:
+                  NetworkImage(FirebaseAuth.instance.currentUser!.photoURL!,
                   ),
-                );
-              },
-              child: CircleAvatar(
-                backgroundImage:
-                NetworkImage(FirebaseAuth.instance.currentUser!.photoURL!,
                 ),
               ),
             ),
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Expanded(
-              child: StreamBuilder<QuerySnapshot<Post>>(
-              stream: postsReference.orderBy('createdAt').snapshots(),
-              builder: (context, snapshot) {
-                final posts = snapshot.data?.docs ??[];
-                return ListView.builder(
-                  itemCount: posts.length,
-                  itemBuilder: (context, index){
-                  final post = posts[index].data();
-                  return Text(post.text);
-                  },
-                );
-              }),
-            ),
-            TextFormField(
-              onFieldSubmitted: (text){
-                final newDocumentReference = postsReference.doc();
-
-                final user = FirebaseAuth.instance.currentUser!;
-                final newPost = Post(
-                  text: text,
-                  createdAt: Timestamp.now(),
-                  posterName: user.displayName!,
-                  posterImageUrl: user.photoURL!,
-                  posterId: user.uid,
-                  reference: postsReference.doc(),
-                );
-
-                newDocumentReference.set(newPost);
-              },
-            ),
           ],
         ),
+        body: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: StreamBuilder<QuerySnapshot<Post>>(
+                stream: postsReference.orderBy('createdAt').snapshots(),
+                builder: (context, snapshot) {
+                  final posts = snapshot.data?.docs ??[];
+                  return ListView.builder(
+                    itemCount: posts.length,
+                    itemBuilder: (context, index){
+                    final post = posts[index].data();
+                    return PostWidget(post: post);
+                    },
+                  );
+                }),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: controller,
+                  decoration: InputDecoration(
+                    hintText: 'テキスト',
+                    fillColor: Colors.amber[100],
+                    filled: true,
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.amber,width: 1,),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.amber,width: 2,),
+                    ),
+                  ),
+                  onFieldSubmitted: (text){
+                    final newDocumentReference = postsReference.doc();
+
+                    final user = FirebaseAuth.instance.currentUser!;
+                    final newPost = Post(
+                      text: text,
+                      createdAt: Timestamp.now(),
+                      posterName: user.displayName!,
+                      posterImageUrl: user.photoURL!,
+                      posterId: user.uid,
+                      reference: postsReference.doc(),
+                    );
+
+                    newDocumentReference.set(newPost);
+                    controller.clear();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PostWidget extends StatelessWidget {
+  const PostWidget({
+    super.key,
+    required this.post,
+  });
+
+  final Post post;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(post.posterImageUrl),
+          ),
+          const SizedBox(width: 4,),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(post.posterName,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                    Text( DateFormat('dd HH:mm').format(post.createdAt.toDate())),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            color: FirebaseAuth.instance.currentUser!.uid == post.posterId ? Colors.amber[100] : Colors.blue[100],
+                          ),
+                          child: Text(post.text),
+                      ),
+                    ),
+                    if (FirebaseAuth.instance.currentUser!.uid == post.posterId)
+                    SizedBox(
+                      width: 96,
+                      child: Row(
+                        children: [
+                          IconButton(
+                              onPressed: (){
+                                post.reference.delete();
+                              },
+                              icon: Icon(Icons.delete)
+                          ),
+                          IconButton(
+                              onPressed: (){
+                               showDialog(
+                                context: context, builder: (context){
+                                  return AlertDialog(
+                                  title: Text('編集する'),
+                                    content: TextFormField(
+                                      initialValue: post.text,
+                                      autofocus: true,
+                                      onFieldSubmitted: (newText){
+                                        post.reference.update(
+                                          {
+                                            'text': newText,
+                                        },
+                                        );
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                    );
+                                   },
+                                  );
+                               },
+                              icon: const Icon(Icons.edit)
+                           ),
+                        ],
+                      ),
+                    )
+                    else
+                      const SizedBox(
+                        width: 96,
+                      )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
